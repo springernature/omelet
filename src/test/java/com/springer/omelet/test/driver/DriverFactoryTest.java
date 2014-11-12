ee632c1927627326129fab456d2b0f04fba50768
borz01 <robert.borzutzki@springer.com>
2014-11-12 14:57:43 +0100

    add tests for enums

---
+++ src/test/java/com/springer/omelet/test/driver/DriverFactoryTest.java
@@ -0,0 +1,21 @@
+package com.springer.omelet.test.driver;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.springer.omelet.data.IBrowserConf;
+//import com.springer.omelet.driver.DriverFactory;
+
+public class DriverFactoryTest {
+	public IBrowserConf iBrowserConf;
+
+	@Test
+	public void f() {
+//		iBrowserConf.getBrowser();
+//		DriverFactory df = new DriverFactory(iBrowserConf);
+//		Assert.assertNotNull(df);
+//		df.intializeDriver();
+//		
+//
+	}
+}
