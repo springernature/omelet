69e923b5de9d7efdf44353f188e0e2dd6a9e884c
kapilag <kapil.aggarwal1@gmail.com>
2014-11-12 20:56:35 +0530

    Adding RefinedBrowserConf and Tests

---
+++ src/main/java/com/springer/omelet/data/RefinedBrowserConf.java
@@ -0,0 +1,76 @@
+package com.springer.omelet.data;
+
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.springer.omelet.common.Utils;
+
+/**
+ * Refine Browser data based on the Hierarchy
+ * default-->Framework-->ClientEnv-->CommandLine
+ * 
+ * @author kapil
+ * 
+ */
+public class RefinedBrowserConf {
+
+	private Map<String, String> clientBrowserData;
+	private boolean isFrameworkProperties;
+	private PropertyValueMin frameworkPropData = null;
+
+	public RefinedBrowserConf(Map<String, String> clientBrowserData,
+			String standBylookUpFileName) {
+		this.clientBrowserData = clientBrowserData;
+		setFrameworkProperties(standBylookUpFileName);
+
+	}
+
+	private void setFrameworkProperties(String standBylookUpFileName) {
+		String pathOfFile = Utils.getResources(RefinedBrowserConf.class,
+				standBylookUpFileName);
+		if (null == pathOfFile) {
+			isFrameworkProperties = false;
+		} else {
+			isFrameworkProperties = true;
+			frameworkPropData = new PropertyValueMin(Utils.getResources(this,
+					standBylookUpFileName));
+		}
+	}
+
+	public String getRefined(String key, String defaultValue) {
+		String refinedValue = defaultValue;
+		refinedValue = getFromFrameworkProp(key, refinedValue);
+		refinedValue = getFromClientEnv(key, refinedValue);
+		refinedValue = getFromJvmArgs(key, refinedValue);
+		return refinedValue;
+	}
+
+	private String getFromFrameworkProp(String key, String value) {
+		if (isFrameworkProperties) {
+			String tempValue = frameworkPropData.getValue(key);
+			if (isNotBlank(tempValue)) {
+				return tempValue;
+			}
+		}
+		return value;
+	}
+
+	private String getFromClientEnv(String key, String value) {
+		String tempValue = clientBrowserData.get(key);
+		if (isNotBlank(tempValue)) {
+			return tempValue;
+		}
+		return value;
+	}
+
+	private boolean isNotBlank(String value) {
+		return StringUtils.isNotBlank(value);
+	}
+
+	private String getFromJvmArgs(String key, String value) {
+		String tempValue = System.getProperty(key);
+		return isNotBlank(tempValue) ? tempValue : value;
+	}
+
+}
---
+++ src/test/java/com/springer/omelet/test/data/RefinedBrowserConfTests.java
@@ -0,0 +1,84 @@
+package com.springer.omelet.test.data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.springer.omelet.common.Utils;
+import com.springer.omelet.data.IProperty;
+import com.springer.omelet.data.PropertyMapping;
+import com.springer.omelet.data.RefinedBrowserConf;
+
+public class RefinedBrowserConfTests {
+	String key = "bsUserName";
+	String key1 = "bsKey";
+	
+	@BeforeMethod
+	public void setup(){
+		System.setProperty(key, "");
+	}
+	
+	@Test
+	public void ifCommandLineAndOtherPresent(){
+		String commandLineValue  = "BrowserStackUserName";
+		Map<String,String> clientMap = new HashMap<String, String>();
+		System.setProperty(key, commandLineValue);
+		clientMap.put(key, "bsUserTest");
+		RefinedBrowserConf rbf = new RefinedBrowserConf(clientMap, "test.properties");
+		Assert.assertEquals(rbf.getRefined("bsUserName", "defaultUserName"), commandLineValue);
+	}
+	
+	@Test
+	public void verifyIfClientEnvPresentAndCommandLineBlank(){
+		String commandLineValue  = "";
+		String clientValue = "clientBsUser";
+		Map<String,String> clientMap = new HashMap<String, String>();
+		System.setProperty(key, commandLineValue);
+		clientMap.put(key,clientValue );
+		RefinedBrowserConf rbf = new RefinedBrowserConf(clientMap, "test.properties");
+		Assert.assertEquals(rbf.getRefined(key, "defaultUserName"), clientValue);
+	}
+	
+	@Test
+	public void verifyIfClient_CommandLine_Blank(){
+		
+		String commandLineValue  = "";
+		String clientValue = "";
+		String defaultValue="defaultUserName";
+		Map<String,String> clientMap = new HashMap<String, String>();
+		System.setProperty(key, commandLineValue);
+		clientMap.put(key,clientValue );
+		RefinedBrowserConf rbf = new RefinedBrowserConf(clientMap, "test.properties");
+		Assert.assertEquals(rbf.getRefined(key, defaultValue), defaultValue);	
+	}
+	@Test
+	public void verifyCommandLineNotPresent(){
+		String clientValue = "clientBsUser";
+		Map<String,String> clientMap = new HashMap<String, String>();
+		clientMap.put(key,clientValue);
+		RefinedBrowserConf rbf = new RefinedBrowserConf(clientMap, "test.properties");
+		Assert.assertEquals(rbf.getRefined(key, "defaultUserName"), clientValue);
+	}
+	
+	@Test
+	public void verifyFrameworkPropertiesValueSet(){
+		String dataFileName = "FrameworkPropTest.properties";
+		Map<String,String> clientMap = new HashMap<String, String>();
+		IProperty frameworkProp = new PropertyMapping(Utils.getResources(this, dataFileName));
+		RefinedBrowserConf rbf = new RefinedBrowserConf(clientMap, dataFileName);
+		Assert.assertEquals(rbf.getRefined(key, "defaultUserName"), frameworkProp.getValue(key));
+	}
+	
+	@Test
+	public void frameworkBlanAndCheckDefault(){
+		String dataFileName = "FrameworkPropTest.properties";
+		String defaultValue = "defaultKey";
+		Map<String,String> clientMap = new HashMap<String, String>();
+		RefinedBrowserConf rbf = new RefinedBrowserConf(clientMap, dataFileName);
+		Assert.assertEquals(rbf.getRefined(key1, defaultValue), defaultValue);
+	}
+
+}
