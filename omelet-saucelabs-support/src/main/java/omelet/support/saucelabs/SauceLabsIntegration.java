135b849aa065028ad0fd491584f875d5199420d0
kapil <kapil.aggarwal1@gmail.com>
2015-07-06 19:48:54 +0530

    Included try catch in sauce integration as ultimate way to catch error which will interfere in test result

--- omelet-saucelabs-support/src/main/java/omelet/support/saucelabs/SauceLabsIntegration.java
+++ omelet-saucelabs-support/src/main/java/omelet/support/saucelabs/SauceLabsIntegration.java
@@ -3,6 +3,7 @@ package omelet.support.saucelabs;
 import omelet.driver.Driver;
 
 import org.apache.log4j.Logger;
+import org.hamcrest.core.IsInstanceOf;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.IInvokedMethod;
 import org.testng.IInvokedMethodListener;
@@ -15,31 +16,40 @@ public class SauceLabsIntegration implements IInvokedMethodListener {
 
 	@Override
 	public void afterInvocation(IInvokedMethod method, ITestResult testResult) {
-		if (null != Driver.getBrowserConf().host() && Driver.getBrowserConf().host().contains("sauce") && Driver.getBrowserConf().isRemoteFlag()) {
-			RemoteWebDriver driver = (RemoteWebDriver) Driver.getDriver();
-			LOGGER.debug("After in SL Integration driver Session ID: " + driver.getSessionId());
-			WebInterface slWebInterface = new WebInterface();
-			slWebInterface.updateSauceLabsJob(driver.getSessionId().toString(),
-					method.getTestMethod().getMethodName(), method
-							.getTestResult().isSuccess());
-			
-			Reporter.setCurrentTestResult(testResult);
-			Reporter.log("SauceLabs Report :: ");
-			Reporter.log(slWebInterface.generateLinkForJob(driver
-					.getSessionId().toString()));
-//			Reporter.log(slWebInterface.generateLinkForEmbedScript(driver
-//					.getSessionId().toString(), true));
-//			Reporter.log("<br>");
-//			 Reporter.log("SauceLabs Video :: "
-//			 + slWebInterface.generateLinkForEmbedScript(driver
-//			 .getSessionId().toString(), false));
-			Reporter.log("<br>");
+		try {
+			// it will throw Null pointer exception for the method who are not
+			// omelet driven , should be fixed as using try catch is a temporary
+			// one!
+			if (Driver.getBrowserConf().host().contains("sauce")
+					&& Driver.getBrowserConf().isRemoteFlag()) {
+				RemoteWebDriver driver = (RemoteWebDriver) Driver.getDriver();
+				LOGGER.debug("After in SL Integration driver Session ID: "
+						+ driver.getSessionId());
+				WebInterface slWebInterface = new WebInterface();
+				slWebInterface.updateSauceLabsJob(driver.getSessionId()
+						.toString(), method.getTestMethod().getMethodName(),
+						method.getTestResult().isSuccess());
+
+				Reporter.setCurrentTestResult(testResult);
+				Reporter.log("SauceLabs Report :: ");
+				Reporter.log(slWebInterface.generateLinkForJob(driver
+						.getSessionId().toString()));
+				// Reporter.log(slWebInterface.generateLinkForEmbedScript(driver
+				// .getSessionId().toString(), true));
+				// Reporter.log("<br>");
+				// Reporter.log("SauceLabs Video :: "
+				// + slWebInterface.generateLinkForEmbedScript(driver
+				// .getSessionId().toString(), false));
+				Reporter.log("<br>");
+			}
+		} catch (Exception e) {
+			LOGGER.error("Not to interfere in the test result!, but needs to be taken care!");
 		}
 	}
 
 	@Override
 	public void beforeInvocation(IInvokedMethod arg0, ITestResult arg1) {
-		
+
 	}
 
 }
